608497278a8d2d6c94751c44a1518b29c860999e
padoindia873-hub <padoindia873@gmail.com>
2025-11-05 11:32:22 +0530

    get_post_delete2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("✅ MongoDB Connected"))
+  .then(() => console.log(" MongoDB Connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // API Routes
@@ -24,4 +24,4 @@ app.use("/api/questions", questionRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
